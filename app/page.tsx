83fcbc0b2bc786b16a4c93be379c2ee8779b7e12
ojela028xamk <jerelans91@gmail.com>
2024-11-22 12:30:03 +0200

    show items

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
 "use client";
+import { useState } from "react";
 import css from "./page.module.scss";
 import { getProductList } from "./services/databaseService";
 import { useEffectOnce } from "react-use";
 
 export default function Home() {
+  const [productList, setProductList] = useState<any[]>([]);
+
   useEffectOnce(() => {
     getProductList()
-      .then((res) => console.log(res))
+      .then((res) => setProductList(res))
       .catch((err) => console.log(err));
   });
 
-  return <div className={css.page}></div>;
+  return (
+    <div className={css.page}>
+      <>
+        {productList.map((item: any) => (
+          <h3 key={item.id}>{item.name}</h3>
+        ))}
+      </>
+    </div>
+  );
 }
